eeb00626856c6cac0b94fe265296c9c5dd73155b
adrijana.jovicic@gmail.com <adrijana.jovicic@gmail.com>
2021-06-16 10:25:02 +0200

    changed home page and added firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import firebase from 'firebase';
+import { AuthService } from './shared/services/auth.service';
+
+
 
 @Component({
   selector: 'app-root',
@@ -7,5 +11,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MetalArt';
-  
+
+  constructor(
+    public _authService: AuthService
+    ){}
+    ngOnInit() {
+      firebase.initializeApp({
+        apiKey: 'AIzaSyCpeT7xJP4rPmBY9Vg7FtE96BvPbxhUA3o',
+        authDomain: 'metal-art-dbb29.firebaseapp.com',
+        databaseURL: 'https://metal-art-dbb29-default-rtdb.firebaseio.com',
+        projectId: 'metal-art-dbb29',
+        storageBucket: 'metal-art-dbb29.appspot.com',
+        messagingSenderId: '219922891107',
+        appId: '1:219922891107:web:958f9356455cd0ff9e1356',
+        measurementId: 'G-XECKMC7746',
+      });
+    }
 }
